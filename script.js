5c3d5bf967206a1438d8e422ea896aa3c545d900
Sameer Fidai <40374828+sameerfidai@users.noreply.github.com>
2021-10-06 20:06:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function increment() {
+    let count = document.getElementById("count");
+    count.innerText++;
+}
+
+function reset() {
+    let count = document.getElementById("count");
+    count.innerText = 0;
+}

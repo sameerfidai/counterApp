9a0178c42ce64f4cc1b0305284703eb791b7d093
Sameer Fidai <40374828+sameerfidai@users.noreply.github.com>
2021-10-06 20:55:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+let oldCount = localStorage.getItem("count");
+
+if (oldCount != null) {
+    count.innerText = oldCount;
+}
+
 function increment() {
     let count = document.getElementById("count");
     count.innerText++;
+    localStorage.setItem("count", count.innerText);
 }
 
 function reset() {
     let count = document.getElementById("count");
     count.innerText = 0;
+    localStorage.setItem("count", count.innerText);
 }
